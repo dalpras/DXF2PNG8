8a3352ce6dc3877fad32dfa2ef821b0c7ba84615
DalPraS <stefano@dalpra.info>
2016-07-05 15:46:52 +0200

    Code optimization

--- ExportDXF2PNG24.jsx
+++ ExportDXF2PNG24.jsx
@@ -33,10 +33,16 @@ if (sourceFolder != null) {
 	}
 }
 
+/**
+ * Main processing function.
+ * 
+ * @param {Array} files Array of Files to process.
+ */
 function main(files) {
 	for (var i = 0; i < files.length; i++) {
 		var dxfFile = files[i],
 		pngFile = createPngFile(destFolder, dxfFile);
+		
 		// don't overwrite existing files, skip as soon as possible
 		if (pngFile.exists == true) {
 			continue;
@@ -56,8 +62,12 @@ function main(files) {
 	}
 	alert('Files are saved as PNG in ' + destFolder);
 }
+
 /**
  * Create a png file based on dxf path assigned.
+ * 
+ * @param {string} Target PNG Folder PathItem
+ * @param {File} Source DXF File to convert
  */
 function createPngFile(destFolder, dxfFile) {
 	var matches = dxfFile.name.match(/([^\/]+)\.DXF$/i);
@@ -67,9 +77,7 @@ function createPngFile(destFolder, dxfFile) {
 /**
  * Apply a stroke effect in each element composing the object.
  *
- * @param {int} percent
- *          Percent of stroke increment
- * @returns {void}
+ * @param {int} percent Percent of stroke increment
  */
 function applyStroke(percent) {
 	var percentile = percent / 100,
@@ -90,7 +98,7 @@ function applyStroke(percent) {
 				objPath.strokeWidth = objStrokeWidth * percentile;
 				objPath.strokeColor = black;
 				// objPath.tracingMode = TRACINGMODEBLACKANDWHITE;
-				// objPath,threshold = 128;
+				// objPath.threshold = 128;
 			}
 		}
 		if (myLayer.typename == "PathItem") {
@@ -98,7 +106,7 @@ function applyStroke(percent) {
 			myLayer.strokeWidth = objStrokeWidth * percentile;
 			myLayer.strokeColor = black;
 			// myLayer.tracingMode = TRACINGMODEBLACKANDWHITE;
-			// myLayer,threshold = 128;
+			// myLayer.threshold = 128;
 		}
 	}
 }
@@ -106,9 +114,8 @@ function applyStroke(percent) {
 /**
  * Process the artboards inside the document applying a 30% size increase and exporting the png.
  *
- * @param {File}
- *
- * @returns {void}
+ * @param {File} pngFile Target PNG file
+ * @param {double} size Target size constraints of PNG
  */
 function exportDocToPng(pngFile, size) {
 	if (app.documents.length == 0) {
@@ -120,11 +127,13 @@ function exportDocToPng(pngFile, size) {
 	// scale the image to a minimum width of 750 and 30% increase
 	var objScale = (size / (artBds[idx].artboardRect[2] - artBds[idx].artboardRect[0])) * 100 * 1.3;
 	exportFileToPNG24(pngFile, objScale);
-	// sourceDoc.exportFile(pngFile, ExportType.PNG24, new ExportOptionsPNG24());
 }
 
 /**
  * Exports current document to pngFile as a PNG24 file with specified options.
+ *
+ * @param {File} pngFile Destination png file
+ * @param {double} objScale Scaling for size constraints
  */
 function exportFileToPNG24(pngFile, objScale) {
 	if (app.documents.length > 0) {
@@ -138,44 +147,6 @@ function exportFileToPNG24(pngFile, objScale) {
 		exportOptions.verticalScale = objScale;
 		exportOptions.horizontalScale = objScale;
 		sourceDoc.exportFile(pngFile, type, exportOptions);
-
-		/* 		exportOptions.antiAliasing = true;
-		exportOptions.artBoardClipping = false;
-		//exportOptions.horizontalScale = 350.0; // scaling to 350%
-		exportOptions.saveAsHTML = false;
-		exportOptions.transparency = false;
-		//exportOptions.verticalScale = 350.0; // scaling to 350%
-		sourceDoc.exportFile(pngFile, type, exportOptions);*/
-
 	}
 }
 
-/**
- * Set the PNG saving options of the files.
- *
- * @param {scale} Scale the image to the value specified
- *
- * @returns {ExportOptionsPNG24}
- */
-/* function getPngOptions(scale) {
-// PNG8
-// var pngExportOpts = new ExportOptionsPNG8();
-// pngExportOpts.colorCount = 4;
-
-// PNG24
-// Create the PDFSaveOptions object to set the PDF options
-var pngExportOpts = new ExportOptionsPNG24();
-
-pngExportOpts.antiAliasing = true;
-pngExportOpts.artBoardClipping = false;
-pngExportOpts.saveAsHTML = false;
-pngExportOpts.transparency = true;
-// pngExportOpts.matte = false;
-// pngExportOpts.matteColor = 0, 0, 0;
-// pngExportOpts.transparency = true;
-pngExportOpts.verticalScale = scale;
-pngExportOpts.horizontalScale = scale;
-
-return pngExportOpts;
-}
-*/
