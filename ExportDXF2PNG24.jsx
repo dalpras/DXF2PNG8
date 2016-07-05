c600807c62b1b926fcdb18110cd0a10ca2b6d101
DalPraS <stefano@dalpra.info>
2016-07-05 14:55:15 +0200

    final release working

--- ExportDXF2PNG24.jsx
+++ ExportDXF2PNG24.jsx
@@ -4,7 +4,7 @@
  * @author Stefano Dal Prà
  */
 
-#target illustrator
+// #target illustrator
 
 // The Document used for exporting must be outside the function for
 // avoiding page setup every time.
@@ -22,41 +22,39 @@ var strokePercent = prompt("Choose your stroke line increase in percent (es. 0-i
 // Get the destination to save the files
 var destFolder = Folder.selectDialog('Select the folder where you want to save the converted PNG files.', '~');
 
-// Get all files matching the pattern
-var files = sourceFolder.getFiles(fileType);
-
 // If a valid folder is selected RUN
-if (sourceFolder != null && files.length > 0) {
-	main(files);
-}
-
-// uncomment to suppress Illustrator warning dialogs
-// app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
-
-function main(files) {
+if (sourceFolder != null) {
+	// Get all files matching the pattern
+	var files = sourceFolder.getFiles(fileType);
 	if (files.length > 0) {
-		for (var i = 0; i < files.length; i++) {
-			var dxfFile = files[i],
-			pngFile = createPngFile(destFolder, dxfFile);
-			// don't overwrite existing files, skip as soon as possible
-			if (pngFile.exists == true) {
-				continue;
-			}
-
-			sourceDoc = this.app.open(dxfFile, DocumentColorSpace.RGB);
-
-			applyStroke(strokePercent);
-
-			// convert to png and save
-			exportDocToPng(pngFile, 750);
-
-			// close the original without saving
-			sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
-		}
-		alert('Files are saved as PNG in ' + destFolder);
+		main(files);
 	} else {
 		alert('No matching files found');
 	}
+}
+
+function main(files) {
+	for (var i = 0; i < files.length; i++) {
+		var dxfFile = files[i],
+		pngFile = createPngFile(destFolder, dxfFile);
+		// don't overwrite existing files, skip as soon as possible
+		if (pngFile.exists == true) {
+			continue;
+		}
+		
+		// uncomment to suppress Illustrator warning dialogs
+		app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
+		sourceDoc = app.open(dxfFile, DocumentColorSpace.RGB);
+
+		applyStroke(strokePercent);
+
+		// convert to png and save
+		exportDocToPng(pngFile, 750);
+
+		// close the original without saving
+		sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
+	}
+	alert('Files are saved as PNG in ' + destFolder);
 }
 /**
  * Create a png file based on dxf path assigned.
@@ -84,7 +82,6 @@ function applyStroke(percent) {
 		//if element is compound make a new loop for pathItems
 		if (myLayer.typename == "CompoundPathItem") {
 			for (var u = 0; u < myLayer.pathItems.length; u++) {
-
 				//take actual stroke size
 				var objPath = myLayer.pathItems[u];
 				var objStrokeWidth = objPath.strokeWidth;
@@ -107,29 +104,13 @@ function applyStroke(percent) {
 }
 
 /**
- * Build a RGB Color.
- *
- * @param {int} r
- * @param {int} g
- * @param {int} b
- * @returns {RGBColor}
- */
-function makeColor(r, g, b) {
-	var c = new RGBColor();
-	c.red = r;
-	c.green = g;
-	c.blue = b;
-	return c;
-}
-
-/**
  * Process the artboards inside the document applying a 30% size increase and exporting the png.
  *
  * @param {File}
  *
  * @returns {void}
  */
-function exportDocToPng(pngFile, minWidth) {
+function exportDocToPng(pngFile, size) {
 	if (app.documents.length == 0) {
 		return;
 	}
@@ -137,8 +118,36 @@ function exportDocToPng(pngFile, minWidth) {
 	var idx = artBds.getActiveArtboardIndex();
 
 	// scale the image to a minimum width of 750 and 30% increase
-	var scale = (minWidth / (artBds[idx].artboardRect[2] - artBds[idx].artboardRect[0])) * 100 * 1.3;
-	sourceDoc.exportFile(pngFile, ExportType.PNG24, getPngOptions(scale));
+	var objScale = (size / (artBds[idx].artboardRect[2] - artBds[idx].artboardRect[0])) * 100 * 1.3;
+	exportFileToPNG24(pngFile, objScale);
+	// sourceDoc.exportFile(pngFile, ExportType.PNG24, new ExportOptionsPNG24());
+}
+
+/**
+ * Exports current document to pngFile as a PNG24 file with specified options.
+ */
+function exportFileToPNG24(pngFile, objScale) {
+	if (app.documents.length > 0) {
+		var exportOptions = new ExportOptionsPNG24();
+		var type = ExportType.PNG24;
+
+		exportOptions.antiAliasing = true;
+		// exportOptions.artBoardClipping = false;
+		exportOptions.transparency = true;
+		// exportOptions.saveAsHTML = false;
+		exportOptions.verticalScale = objScale;
+		exportOptions.horizontalScale = objScale;
+		sourceDoc.exportFile(pngFile, type, exportOptions);
+
+		/* 		exportOptions.antiAliasing = true;
+		exportOptions.artBoardClipping = false;
+		//exportOptions.horizontalScale = 350.0; // scaling to 350%
+		exportOptions.saveAsHTML = false;
+		exportOptions.transparency = false;
+		//exportOptions.verticalScale = 350.0; // scaling to 350%
+		sourceDoc.exportFile(pngFile, type, exportOptions);*/
+
+	}
 }
 
 /**
@@ -148,24 +157,25 @@ function exportDocToPng(pngFile, minWidth) {
  *
  * @returns {ExportOptionsPNG24}
  */
-function getPngOptions(scale) {
-	// PNG8
-	// var pngExportOpts = new ExportOptionsPNG8();
-	// pngExportOpts.colorCount = 4;
+/* function getPngOptions(scale) {
+// PNG8
+// var pngExportOpts = new ExportOptionsPNG8();
+// pngExportOpts.colorCount = 4;
 
-	// PNG24
-	// Create the PDFSaveOptions object to set the PDF options
-	var pngExportOpts = new ExportOptionsPNG24();
+// PNG24
+// Create the PDFSaveOptions object to set the PDF options
+var pngExportOpts = new ExportOptionsPNG24();
 
-	pngExportOpts.antiAliasing = true;
-	pngExportOpts.artBoardClipping = false;
-	pngExportOpts.saveAsHTML = false;
-	pngExportOpts.transparency = true;
-	// pngExportOpts.matte = false;
-	// pngExportOpts.matteColor = 0, 0, 0;
-	// pngExportOpts.transparency = true;
-	pngExportOpts.verticalScale = scale;
-	pngExportOpts.horizontalScale = scale;
+pngExportOpts.antiAliasing = true;
+pngExportOpts.artBoardClipping = false;
+pngExportOpts.saveAsHTML = false;
+pngExportOpts.transparency = true;
+// pngExportOpts.matte = false;
+// pngExportOpts.matteColor = 0, 0, 0;
+// pngExportOpts.transparency = true;
+pngExportOpts.verticalScale = scale;
+pngExportOpts.horizontalScale = scale;
 
-	return pngExportOpts;
+return pngExportOpts;
 }
+*/
